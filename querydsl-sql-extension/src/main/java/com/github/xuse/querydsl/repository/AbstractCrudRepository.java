577b6ec3cda3688e303ac80786b13c233e47bf92
xuse <easyframe@163.com>
2026-05-20 18:56:54 +0800

    Remove method initPath() from GenericRepository.

--- querydsl-sql-extension/src/main/java/com/github/xuse/querydsl/repository/AbstractCrudRepository.java
+++ querydsl-sql-extension/src/main/java/com/github/xuse/querydsl/repository/AbstractCrudRepository.java
@@ -1,11 +1,15 @@
 package com.github.xuse.querydsl.repository;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -34,6 +38,7 @@ import com.github.xuse.querydsl.sql.expression.AdvancedMapper;
 import com.github.xuse.querydsl.sql.expression.BeanCodec;
 import com.github.xuse.querydsl.sql.expression.BeanCodecManager;
 import com.github.xuse.querydsl.sql.expression.Property;
+import com.github.xuse.querydsl.sql.support.SQLTypeUtils;
 import com.github.xuse.querydsl.util.ArrayUtils;
 import com.github.xuse.querydsl.util.Exceptions;
 import com.github.xuse.querydsl.util.StringUtils;
@@ -54,6 +59,9 @@ import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.sql.PrimaryKey;
 import com.querydsl.sql.RelationalPath;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public abstract class AbstractCrudRepository<T, ID> implements CRUDRepository<T, ID> {
 	@Override
 	public T load(ID key) {
@@ -1044,4 +1052,75 @@ public abstract class AbstractCrudRepository<T, ID> implements CRUDRepository<T,
 		}
 		return (SimpleExpression<P>)p;
 	}
+	
+	@SuppressWarnings("unchecked")
+	protected RelationalPath<T> calcPathType() {
+		Class<?> c= this.getClass();
+		Map<TypeVariable<?>,Type> context=new LinkedHashMap<>();
+		while (c != Object.class) {
+			Type s = c.getGenericSuperclass();
+			if (s instanceof ParameterizedType) {
+				ParameterizedType p = (ParameterizedType) s;
+				TypeVariable<?>[] vars = c.getSuperclass().getTypeParameters();
+				Type[] typeArgs = p.getActualTypeArguments();
+				//维护泛型变量上下文
+				for (int i = 0; i < vars.length; i++) {
+					TypeVariable<?> key = vars[i];
+					Type value = typeArgs[i];
+					if (value instanceof TypeVariable<?>) {
+						value = typeArgs[i] = context.get((TypeVariable<?>) value);
+						context.put(key, value);
+					} else {
+						context.put(key, value);
+					}
+				}
+				if (p.getRawType() == GenericRepository.class) {
+					Type gT = typeArgs[0];
+					if (gT instanceof Class) {
+						return getPath((Class<T>) gT);
+					}
+				}
+				
+				
+			}
+			c = c.getSuperclass();
+		}
+		for (Type t : this.getClass().getGenericInterfaces()) {
+			if(!(t instanceof ParameterizedType)) {
+				continue;
+			}
+			ParameterizedType p = (ParameterizedType) t;
+			if (p.getRawType() != CRUDRepository.class) {
+				continue;
+			}
+			Type gT = p.getActualTypeArguments()[0];
+			if (!(gT instanceof Class) ){
+				continue;
+			}
+			Class<T> entity = (Class<T>) gT;
+			return getPath(entity);
+		}
+		throw Exceptions.illegalArgument("No relational path found in {}", getClass().getName());
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	private RelationalPath<T> getPath(Class<T> entity) {
+		String name = entity.getName();
+		int index = name.lastIndexOf('.');
+		String qClassName = name.substring(0, index + 1) + "Q" + name.substring(index + 1);
+		Class<?> clz;
+		try {
+			clz = entity.getClassLoader().loadClass(qClassName);
+		} catch (ClassNotFoundException e) {
+			log.warn("Query Class not found {}, will generate a dynamic model.", qClassName);
+			return PathCache.get(entity, null);
+		}
+		RelationalPath<?> result= SQLTypeUtils.getMetaModel(clz);
+		if(result==null) {
+			throw Exceptions.illegalArgument("No relational path found in {}", clz.getName());
+		}
+		return (RelationalPath<T>) result;
+	}
+
 }
--- querydsl-sql-extension/src/main/java/com/github/xuse/querydsl/repository/GenericRepository.java
+++ querydsl-sql-extension/src/main/java/com/github/xuse/querydsl/repository/GenericRepository.java
@@ -1,17 +1,8 @@
 package com.github.xuse.querydsl.repository;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import javax.annotation.Resource;
 
-import com.github.xuse.querydsl.lambda.PathCache;
 import com.github.xuse.querydsl.sql.SQLQueryFactory;
-import com.github.xuse.querydsl.sql.support.SQLTypeUtils;
-import com.github.xuse.querydsl.util.Exceptions;
 import com.querydsl.sql.RelationalPath;
 
 import lombok.extern.slf4j.Slf4j;
@@ -31,83 +22,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class GenericRepository<T, ID> extends AbstractCrudRepository<T, ID> {
 	@Resource
-	protected SQLQueryFactory factory;
+	private SQLQueryFactory factory;
 	
 	protected final RelationalPath<T> path;
 	
 	public GenericRepository() {
-		path = initPath();
+		path = calcPathType();
 	}
-
-	@SuppressWarnings("unchecked")
-	private RelationalPath<T> initPath() {
-		Class<?> c= this.getClass();
-		Map<TypeVariable<?>,Type> context=new LinkedHashMap<>();
-		while (c != Object.class) {
-			Type s = c.getGenericSuperclass();
-			if (s instanceof ParameterizedType) {
-				ParameterizedType p = (ParameterizedType) s;
-				TypeVariable<?>[] vars = c.getSuperclass().getTypeParameters();
-				Type[] typeArgs = p.getActualTypeArguments();
-				//维护泛型变量上下文
-				for (int i = 0; i < vars.length; i++) {
-					TypeVariable<?> key = vars[i];
-					Type value = typeArgs[i];
-					if (value instanceof TypeVariable<?>) {
-						value = typeArgs[i] = context.get((TypeVariable<?>) value);
-						context.put(key, value);
-					} else {
-						context.put(key, value);
-					}
-				}
-				if (p.getRawType() == GenericRepository.class) {
-					Type gT = typeArgs[0];
-					if (gT instanceof Class) {
-						return getPath((Class<T>) gT);
-					}
-				}
-				
-				
-			}
-			c = c.getSuperclass();
-		}
-		for (Type t : this.getClass().getGenericInterfaces()) {
-			if(!(t instanceof ParameterizedType)) {
-				continue;
-			}
-			ParameterizedType p = (ParameterizedType) t;
-			if (p.getRawType() != CRUDRepository.class) {
-				continue;
-			}
-			Type gT = p.getActualTypeArguments()[0];
-			if (!(gT instanceof Class) ){
-				continue;
-			}
-			Class<T> entity = (Class<T>) gT;
-			return getPath(entity);
-		}
-		throw Exceptions.illegalArgument("No relational path found in {}", getClass().getName());
-	}
-
-	@SuppressWarnings("unchecked")
-	private RelationalPath<T> getPath(Class<T> entity) {
-		String name = entity.getName();
-		int index = name.lastIndexOf('.');
-		String qClassName = name.substring(0, index + 1) + "Q" + name.substring(index + 1);
-		Class<?> clz;
-		try {
-			clz = entity.getClassLoader().loadClass(qClassName);
-		} catch (ClassNotFoundException e) {
-			log.warn("Query Class not found {}, will generate a dynamic model.", qClassName);
-			return PathCache.get(entity, null);
-		}
-		RelationalPath<?> result= SQLTypeUtils.getMetaModel(clz);
-		if(result==null) {
-			throw Exceptions.illegalArgument("No relational path found in {}", clz.getName());
-		}
-		return (RelationalPath<T>) result;
-	}
-
 	@Override
 	protected RelationalPath<T> getPath() {
 		return path;
